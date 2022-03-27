faac8cb20aa2faca683bc17a3a177c805bac9e29
chacham <ccm0702@naver.com>
2022-03-27 21:46:11 +0900

    Use expandable to split completed/incompleted schedules

--- lib/views/schedule_list_of_day_page.dart
+++ lib/views/schedule_list_of_day_page.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:dongbaek/blocs/progress_bloc.dart';
 import 'package:dongbaek/blocs/schedule_bloc.dart';
 import 'package:dongbaek/blocs/snapshot_bloc.dart';
@@ -7,6 +5,7 @@ import 'package:dongbaek/blocs/timer_bloc.dart';
 import 'package:dongbaek/models/schedule.dart';
 import 'package:dongbaek/models/snapshot.dart';
 import 'package:dongbaek/utils/datetime_utils.dart';
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -40,36 +39,25 @@ class _ScheduleListOfDayPageState extends State<ScheduleListOfDayPage> {
               "${_currentDate.year}/${_currentDate.month}/${_currentDate.day}(${DateTimeUtils.getDayOfWeek(_currentDate)})"),
         ),
         body: BlocBuilder<SnapshotBloc, List<Snapshot>>(builder: (context, List<Snapshot> snapshots) {
-          final tiles = snapshots.map((snapshot) {
-            final schedule = snapshot.schedule;
-            final progress = snapshot.progress;
-            final repeatInfo = schedule.repeatInfo;
-            Text subtitle;
-            if (schedule.repeatInfo is RepeatPerDay) {
-              subtitle = Text('${progress.completeTimes.length} / ${(repeatInfo as RepeatPerDay).repeatCount} ${schedule.startDate}~ ${snapshot.isComplete()}');
-            } else {
-              subtitle = Text('${progress.completeTimes.length} / ${(repeatInfo as RepeatPerWeek).repeatCount} ${schedule.startDate}~ ${snapshot.isComplete()}');
-            }
-            return ListTile(
-              title: Text(schedule.title + " by " + (schedule.repeatInfo is RepeatPerDay ? "Daily" : "Weekly")),
-              subtitle: subtitle,
-              trailing: IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () {
-                  context.read<ScheduleBloc>().add(RemoveSchedule(schedule.id!));
-                  context.read<SnapshotBloc>().add(const SnapshotDataUpdated());
-                },
-              ),
-              onLongPress: () {
-                context.read<ProgressBloc>().add(AddProgress(schedule.id!, DateTime.now()));
-                context.read<SnapshotBloc>().add(const SnapshotDataUpdated());
-              },
-            );
-          }).toList();
-          return ListView.builder(
-            itemCount: tiles.length,
-            itemBuilder: (BuildContext context, int index) => Card(child: tiles[index]),
-          );
+          final incompleteTiles = snapshots.where((s) => !s.isComplete()).map(_buildSnapshotTile).toList();
+          final completeTiles = snapshots.where((s) => s.isComplete()).map(_buildSnapshotTile).toList();
+          return ListView(
+              children: List<Widget>.generate(
+                    incompleteTiles.length,
+                    (index) => Card(child: incompleteTiles[index]),
+                  ) +
+                  [
+                    ExpandablePanel(
+                      header: const ListTile(title: Text("Completed Schedules")),
+                      collapsed: Container(),
+                      expanded: Column(
+                        children: List.generate(
+                          completeTiles.length,
+                          (int index) => Card(child: completeTiles[index]),
+                        ),
+                      ),
+                    )
+                  ]);
         }),
         floatingActionButton: FloatingActionButton(
           onPressed: () async {
@@ -79,6 +67,35 @@ class _ScheduleListOfDayPageState extends State<ScheduleListOfDayPage> {
           child: const Icon(Icons.add),
         ),
       ),
+    );
+  }
+
+  Widget _buildSnapshotTile(Snapshot snapshot) {
+    final schedule = snapshot.schedule;
+    final progress = snapshot.progress;
+    final repeatInfo = schedule.repeatInfo;
+    Text subtitle;
+    if (schedule.repeatInfo is RepeatPerDay) {
+      subtitle = Text(
+          '${progress.completeTimes.length} / ${(repeatInfo as RepeatPerDay).repeatCount} ${schedule.startDate}~ ${snapshot.isComplete()}');
+    } else {
+      subtitle = Text(
+          '${progress.completeTimes.length} / ${(repeatInfo as RepeatPerWeek).repeatCount} ${schedule.startDate}~ ${snapshot.isComplete()}');
+    }
+    return ListTile(
+      title: Text(schedule.title + " by " + (schedule.repeatInfo is RepeatPerDay ? "Daily" : "Weekly")),
+      subtitle: subtitle,
+      trailing: IconButton(
+        icon: const Icon(Icons.more_vert),
+        onPressed: () {
+          context.read<ScheduleBloc>().add(RemoveSchedule(schedule.id!));
+          context.read<SnapshotBloc>().add(const SnapshotDataUpdated());
+        },
+      ),
+      onLongPress: () {
+        context.read<ProgressBloc>().add(AddProgress(schedule.id!, DateTime.now()));
+        context.read<SnapshotBloc>().add(const SnapshotDataUpdated());
+      },
     );
   }
 }
